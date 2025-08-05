b906306edf36e7963a7672589a3714d6395b0374
Clem-Coding <clementine.blondeau@gmail.com>
2025-08-05 12:35:50 +0200

    Add users and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,32 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher) {}
+
     public function load(ObjectManager $manager): void
     {
+        //Création des utilisateurs
+        $usersData = [
+            ['email' => 'user@bookapi.com', 'roles' => ['ROLE_USER']],
+            ['email' => 'admin@bookapi.com', 'roles' => ['ROLE_ADMIN']],
+        ];
+
+        foreach ($usersData as $data) {
+            $user = new User();
+            $user->setEmail($data['email']);
+            $user->setRoles($data['roles']);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+            $manager->persist($user);
+        }
+
+        //Création des auteurs
         $authorList = [];
         for ($i = 0; $i < 10; $i++) {
             $author = new Author();
@@ -20,7 +39,7 @@ class AppFixtures extends Fixture
             $authorList[] = $author;
         }
 
-
+        //Création des livres
         for ($i = 0; $i < 20; $i++) {
             $book = new Book();
             $book->setTitle('Titre ' . $i);
